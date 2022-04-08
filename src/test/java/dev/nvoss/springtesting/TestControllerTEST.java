51c96b8418909bc6ad475367cd492c0ae8ad3afd
Nick <nvoss@me.com>
2022-04-08 18:36:33 -0500

    Initial commit

---
+++ src/test/java/dev/nvoss/springtesting/TestControllerTEST.java
@@ -0,0 +1,36 @@
+package dev.nvoss.springtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class TestControllerTEST {
+
+    @SpringBootTest
+    @AutoConfigureMockMvc
+    public class HelloControllerTest {
+
+        /*
+        lets you, through a set of convenient builder classes, send HTTP requests
+        into the DispatcherServlet and make assertions about the result.
+         */
+        @Autowired
+        private MockMvc mvc;
+
+        @Test
+        public void getHello() throws Exception {
+            mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+        }
+    }
+
+}
